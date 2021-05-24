22b48ed85472c36fac4283ede5a950b5428607f4
tahj <tahjharris00@gmail.com>
2021-05-24 08:05:06 -0500

    work on login functionality

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI
+const bodyParser = require('body-parser');
+const router = express.Router();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+router.get("/test", (req, res) => res.json({ msg: "This is the tweets route" }));
+
+module.exports = router;
 
 mongoose
     .connect(db, { useNewUrlParser: true })
